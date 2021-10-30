caf16bd7d9d0de300d1fd17d849b43ac750b73c8
JcaveroR <jcaveror@gmail.com>
2021-10-30 14:45:53 -0500

    Update Sidebar.jsx
    cambio de categorias

--- src/components/commons/Sidebar.jsx
+++ src/components/commons/Sidebar.jsx
@@ -31,12 +31,12 @@ export default function Sidebar({ showSidebar, setSidebar }) {
       zIndex="1100"
     >
       <RadioButtonGroup defaultValue="all" value={category} onChange={(val) => setCategory(val)} isInline>
-        <CustomRadio value="all" title="All Products" icon={<Box as={BiMinus} size="24px" mr="10" />} />
-        <CustomRadio value="fruits" title="Fruits" icon={<Box as={BiMinus} size="24px" mr="10" />} />
-        <CustomRadio value="vegetable" title="Vegetables" icon={<Box as={BiMinus} size="24px" mr="10" />} />
-        <CustomRadio value="meat" title="Meats" icon={<Box as={BiMinus} size="24px" mr="10" />} />
-        <CustomRadio value="bakery" title="Bakery" icon={<Box as={BiMinus} size="24px" mr="10" />} />
-        <CustomRadio value="drink" title="Drinks" icon={<Box as={BiMinus} size="24px" mr="10" />} />
+        <CustomRadio value="all" title="Todos los Product0s" icon={<Box as={BiMinus} size="24px" mr="10" />} />
+        <CustomRadio value="fruits" title="Frescos" icon={<Box as={BiMinus} size="24px" mr="10" />} />
+        <CustomRadio value="vegetable" title="Crocantes" icon={<Box as={BiMinus} size="24px" mr="10" />} />
+        <CustomRadio value="meat" title="Criollo" icon={<Box as={BiMinus} size="24px" mr="10" />} />
+        <CustomRadio value="bakery" title="Postres" icon={<Box as={BiMinus} size="24px" mr="10" />} />
+        <CustomRadio value="drink" title="Bebidas" icon={<Box as={BiMinus} size="24px" mr="10" />} />
       </RadioButtonGroup>
     </Box>
   );
