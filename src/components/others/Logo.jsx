c64ceca47efcde28ebf38350e950cc95846ebdcf
S4cK <jcaveror@gmail.com>
2021-11-02 14:15:19 -0500

    segundo logo

--- src/components/others/Logo.jsx
+++ src/components/others/Logo.jsx
@@ -44,7 +44,7 @@ export default function Logo({ width = 100 }) {
               transform="translate(-1.16 34.4)"
               fill="url(#linear-gradient-2)" */
 
-              transform="translate(2.65 5.4)"
+              transform="translate(1.85 20.4)"
               fill="url(#linear-gradient)"
               
             />
