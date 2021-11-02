ba91d6934db10604261d02c88aa864bb3d5cf17d
S4cK <jcaveror@gmail.com>
2021-11-02 15:47:00 -0500

    segundo logo

--- src/components/commons/Sidebar.jsx
+++ src/components/commons/Sidebar.jsx
@@ -31,7 +31,7 @@ export default function Sidebar({ showSidebar, setSidebar }) {
       zIndex="1100"
     >
       <RadioButtonGroup defaultValue="all" value={category} onChange={(val) => setCategory(val)} isInline>
-        <CustomRadio value="all" title="Todos los Product0s" icon={<Box as={BiMinus} size="24px" mr="10" />} />
+        <CustomRadio value="all" title="Todos los Productos" icon={<Box as={BiMinus} size="24px" mr="10" />} />
         <CustomRadio value="ceviche" title="Los Frescos" icon={<Box as={BiMinus} size="24px" mr="10" />} />
         <CustomRadio value="crocante" title="Los Crocantes" icon={<Box as={BiMinus} size="24px" mr="10" />} />
         <CustomRadio value="caliente" title="Sopas" icon={<Box as={BiMinus} size="24px" mr="10" />} />
